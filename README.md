4d4cbc9334b62de5617822d4519d36377e2b1749
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2023-12-28 17:39:21 -0500

    add icon and audio files, add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Stock Price Notifier
+A stock price desktop notifier built in Python. Compatible with MacOS, Linux, and Windows.
+
+## Libraries used
+- [Yahoo Finance](https://pypi.org/project/yahoo-fin/)
+- [notify-py](https://pypi.org/project/notify-py/)
+
+## Credits
+- App icon: https://icons8.com/icon/2dT788URbae8/stocks-growth
+- Notification sound: https://freesound.org/people/SpiceProgram/sounds/399192/
--- notifier.py
+++ notifier.py
@@ -1,6 +1,13 @@
 import platform
 from notifypy import Notify
-notification = Notify(default_notification_title="Stock Notifier")
+import os
+
+icon = os.path.join(os.getcwd(), "stock-96.png")
+audio = os.path.join(os.getcwd(), "notification.wav")
+notification = Notify(default_notification_title="Stock Price Notifier",
+                      default_application_name="Stock Price Notifier", default_notification_icon=icon,
+                      default_notification_audio=audio)
+
 
 def os_notify(message):
     notification.message = message
